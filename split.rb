feb46a64f4fb2b4e6d66a9d97c90f9d31c4ea019
Sheldon Johnson <shayolden@hotmail.com>
2021-05-02 00:59:01 +1000

    Fix broken regex

--- split.rb
+++ split.rb
@@ -3,45 +3,52 @@ require "pry"
 
 PAK0_FILENAMES = File.readlines("pak0.txt", chomp: true)
 PAK1_FILENAMES = File.readlines("pak1.txt", chomp: true)
+# TF_PAK0_FILENAMES = File.readlines("pak1.txt", chomp: true)
+
 ASSET_SUBDIRS = {
-  "mdl" => "progs",
+  "mdl" => nil,
   "bsp" => "progs",
-  "loc" => "locs",
-  "lit" => "lits",
   "wav" => "sound"
 }
 
-IGNORED_DEPS = %w(
-Doomtex.wad
-progs/soldier.mdl
-)
+DEST_PATH = "/tmp/pk3_builder"
 
-Dir.glob("fortress/maps/*.ent") do |filename|
-  ent_file = IO.read(filename).encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
-  map_name = File.basename(filename, File.extname(filename))
-  FileUtils.mkdir_p("/tmp/pk3_builder/#{map_name}")
+Dir.glob(["fortress", "maps", "*.ent"].join("/")) do |filename|
+  puts map_name = File.basename(filename, File.extname(filename))
 
-  dependency_filenames = ent_file.scan(/([a-zA-Z0-9\/]+\.[a-zA-Z0-9]{3})/).flatten
+  FileUtils.mkdir_p([DEST_PATH, map_name, "maps"].join("/"))
+  FileUtils.cp(filename, [DEST_PATH, map_name, "maps", "#{map_name}.ent"].join("/"))
+  FileUtils.cp(filename, [DEST_PATH, map_name, "maps", "#{map_name}.bsp"].join("/"))
 
-  dependency_filenames.each do |dep|
-    next if IGNORED_DEPS.include?(dep)
-    ext = File.extname(dep).delete(".")
+  dependency_filenames = IO.read(filename)
+    .encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+    .scan(/\w[\w\/]*\.(?:mdl|bsp|wav)/)
 
-    subdir = ASSET_SUBDIRS.fetch(ext, "progs")
-    source_file = "fortress/#{subdir}/#{dep}"
-    dest_path = "/tmp/pk3_builder/#{map_name}/#{subdir}/"
-    FileUtils.mkdir_p(dest_path)
+  if dependency_filenames
+    dependency_filenames.flatten.uniq.each do |dep|
+      puts dep
+      ext = File.extname(dep).delete(".")
+      if ASSET_SUBDIRS[ext]
+        subdir = ASSET_SUBDIRS.fetch(ext, "progs")
+        source_file = ["fortress", subdir, dep].join("/")
+        dest_path = [DEST_PATH, map_name, subdir].join("/")
+      else
+        subdir = nil
+        source_file = ["fortress", dep].join("/")
+        dest_path = [DEST_PATH, map_name].join("/")
+      end
 
-    puts source_file
-    puts dest_path
-    puts
+      FileUtils.mkdir_p(dest_path)
 
-    if File.file?(source_file)
-      FileUtils.cp(source_file, dest_path)
-    else
-      File.open("/tmp/pk3_builder/#{map_name}/missing.txt", "a") do |f|
-        f.puts("#{subdir}/#{dep}")
+      if File.file?(source_file)
+        FileUtils.cp(source_file, dest_path)
+      else
+        File.open([DEST_PATH, map_name, "missing.txt"].join("/"), "a") do |f|
+          f.puts([subdir, dep].compact.join("/"))
+        end
       end
     end
   end
+
+  puts
 end
