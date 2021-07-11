05f6fc62931b1e579a03abb1a44a6148bfb88960
Sheldon Johnson <shayolden@hotmail.com>
2021-07-12 02:44:16 +1000

    Debug actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo ${AWS_SECRET_ACCESS_KEY}
+echo ${AWS_ACCESS_KEY_ID}
+echo ${S3_PACKAGE_URI}
+
 if [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] && [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${S3_PACKAGE_URI}" ]; then
   /usr/local/bin/aws s3 sync /map-repo/package ${S3_PACKAGE_URI}
 fi
