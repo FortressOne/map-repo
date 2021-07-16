d9c2ebf7d9b2543a8feaa7729061526da376a296
Sheldon Johnson <shayolden@hotmail.com>
2021-07-17 04:14:37 +1000

    Save of transfer costs by not re-uploading the exact same files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] && [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_S3_PACKAGE_BUCKET}" ]; then
-  /usr/local/bin/aws s3 sync /fortress/maps ${AWS_S3_PACKAGE_BUCKET}
+  /usr/local/bin/aws s3 sync --size-only /fortress/maps ${AWS_S3_PACKAGE_BUCKET}
 fi
