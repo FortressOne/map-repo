fd6c423a84e23c7ed2c54ca19619e0125a8fd0ca
Sheldon Johnson <shayolden@hotmail.com>
2023-06-16 19:58:35 +1000

    Lets try package the files one at a time and delete the source as we go to keep hd space requirements lower for github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 if [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] && [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_S3_PACKAGE_BUCKET}" ]; then
+  for package_path in fortress/package/*; do
+    ./package_bsp ${package_path}
+    rm -rf ${package_path}
+  done
+
   /usr/local/bin/aws s3 sync --size-only /fortress/maps ${AWS_S3_PACKAGE_BUCKET} --delete
 fi
